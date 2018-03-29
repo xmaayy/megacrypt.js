cb540142ee03083583e9de4d1529ab03b338a63e
root <root@DESKTOP-UH0PCQV.localdomain>
2018-03-29 14:04:22 -0400

    Comments and minor readability changes

--- modules/megacrypt.js
+++ modules/megacrypt.js
@@ -8,9 +8,14 @@ const config = require('../config')
 
 class Megacrypt {
   constructor () {
+    //Arrow functions for encrypting and decrypting strings with CryptoJS
     this.encryptServer = str => CryptoJS.AES.encrypt(str, config.serverKey).toString()
     this.decryptServer = str => CryptoJS.AES.decrypt(str, config.serverKey).toString(CryptoJS.enc.Utf8)
+
+    //
     this.encryptUrl = (url, host, callback) => {
+      //A complicated RegExp to see if the 'url' parameter that was passed
+      //is (I've never seen this form in the url so IDK)
       if (/\/\/mega\.nz\/#![\d\w]+![\d\w-]+/.test(url)) {
         let response = {}
         let [, fileId, fileKey] = url.split('!')
@@ -19,6 +24,11 @@ class Megacrypt {
         let link = this.encryptServer(`${crypt}!${cryptKey}`)
         response.link = `http://${host}/dl/_/${base64url.escape(link)}`
 
+        //Test Logging
+        console.log('url: ' + url)
+        console.log('FileID, FileKey: ' + fileId + ' , ' + fileKey)
+        console.log('Crypt: ' + crypt)
+
         let file = new mega.File({downloadId: fileId, key: fileKey, directory: false})
         file.loadAttributes((err, file) => {
           if (err) throw err
@@ -26,6 +36,8 @@ class Megacrypt {
           response.size = bytes(file.size)
           callback([response])
         })
+      //A complicated RegExp to see if the 'url' parameter that was passed
+      //is in fact a folder
       } else if (/\/\/mega\.nz\/#F![\d\w]+![\d\w-]+/.test(url)) {
         let [, downloadId, fileKey] = url.split('!')
         let folder = new mega.File({downloadId: downloadId, key: fileKey, directory: true})
@@ -36,6 +48,7 @@ class Megacrypt {
           let pushFiles = file => file.directory ? async.each(file.children, pushFiles, console.log) : files.push(file)
           async.each(folder.children, pushFiles, console.log)
           async.each(files, file => {
+            //If the current file is not a directory
             if (!file.directory) {
               let response = {}
               let cryptKey = crypto.randomBytes(32).toString('base64')
@@ -45,21 +58,32 @@ class Megacrypt {
               response.name = file.name
               response.size = bytes(file.size)
               links.push(response)
+              //Test Logging
+              console.log('url: ' + url)
+              console.log('FileID, FileKey: ' + downloadId + ' , ' + fileKey)
+              console.log('Crypt: ' + crypt)
+              console.log(response.link)
             }
           }, console.log)
           callback(links)
         })
       }
     }
+    //Decrypt the encrypted url with our internal secret key
     this.decryptUrl = (crypt, type) => {
       if (type === '_') {
         let [decrypt, key] = this.decryptServer(base64url.unescape(crypt)).split('!')
         let [fileId, fileKey] = CryptoJS.AES.decrypt(decrypt, key).toString(CryptoJS.enc.Utf8).split('!')
         return {fileId: fileId, fileKey: fileKey}
       } else if (type === '!') {
+        //Decrypt URL
         let [decrypt, key] = this.decryptServer(base64url.unescape(crypt)).split('!')
-        let [fileId, folderId, fileKey] = CryptoJS.AES.decrypt(base64url.unescape(decrypt), base64url.unescape(key))
+
+        //Decrypt key
+        let [fileId, folderId, fileKey] = 
+        CryptoJS.AES.decrypt(base64url.unescape(decrypt), base64url.unescape(key)) 
         .toString(CryptoJS.enc.Utf8).split('!')
+
         return {fileId: fileId, folderId: folderId, fileKey: fileKey}
       }
     }
--- routes/download.js
+++ routes/download.js
@@ -4,9 +4,14 @@ const router = express.Router()
 const megacrypt = require('../modules/megacrypt.js')
 const config = require('../config')
 
+//Ge params from url
+//Type: Either '!' or '_', ! indicates folder, _ indicates a single file
 router.get('/:type/:crypt', function (req, res, next) {
+
+  //Decrypt megaurl with custom megacrypt module
   let decrypt = megacrypt.decryptUrl(req.params.crypt, req.params.type)
 
+  //Dont know when this will be the case, but this is just a straight download
   if (req.params.type === '_') {
     let file = new mega.File({downloadId: decrypt.fileId, key: decrypt.fileKey, directory: false})
     file.loadAttributes((err, file) => {
@@ -15,20 +20,26 @@ router.get('/:type/:crypt', function (req, res, next) {
       res.setHeader('Content-Disposition', `attachment; filename="${file.name}"`)
       file.download({returnCiphertext: config.returnCiphertext}).pipe(res)
     })
+  //This is the most common type
   } else if (req.params.type === '!') {
     let folder = new mega.File({downloadId: decrypt.folderId, key: decrypt.fileKey, directory: true})
     folder.loadAttributes((err, folder) => {
       if (err) throw err
+
+      //Set resource headers and pipe download file to user
       let downloadFile = file => {
         res.setHeader('Content-Length', file.size)
         res.setHeader('Content-Disposition', `attachment; filename="${file.name}"`)
         file.download({returnCiphertext: config.returnCiphertext}).pipe(res)
       }
+
+      //If the arg is not a folder, check if the file is the one we want
+      //If both true, start the download process
+      //Otherwise recursively traverse dirs to get links to all files
       let findFile = file => {
-        if (!file.directory) {
-          if (file.downloadId.toString() === decrypt.fileId) {
+        if (!file.directory && file.downloadId.toString() === decrypt.fileId) {
+            console.log(file.downloadId.toString())
             return downloadFile(file)
-          }
         } else {
           file.children.forEach(findFile)
         }
